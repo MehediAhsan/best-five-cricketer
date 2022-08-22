623c6fbd277e78b2c97b02e6123d8c8b5fb4bddd
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-08-23 03:52:45 +0600

    select only five player

--- js/script.js
+++ js/script.js
@@ -11,7 +11,13 @@ const buttons = document.getElementsByClassName('btn-select');
 for (const button of buttons){
     button.addEventListener('click', function(event){
         const playerName = event.target.parentNode.firstElementChild.innerText;
+        if(playersName.length>4){
+            alert('You can not select more than five player');
+            return;
+        }
         playersName.push(playerName);
-        display(playersName);       
+        display(playersName);
+        button.disabled=true;
+        button.style.backgroundColor = 'gray';       
     });
 }
