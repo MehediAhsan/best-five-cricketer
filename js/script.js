b06d78b14d31f4c60d5efb4fb5f9131970e2c0ff
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-08-23 14:18:14 +0600

    per player cost calculation

--- js/script.js
+++ js/script.js
@@ -21,3 +21,23 @@ for (const button of buttons){
         button.style.backgroundColor = 'gray';       
     });
 }
+
+function getInputFieldAmount(input){
+    const inputField = document.getElementById(input);
+    const inputAmount = parseFloat(inputField.value);
+    return inputAmount;     
+}
+
+document.getElementById('btn-calculate').addEventListener('click', function(){
+    const playerPrice = getInputFieldAmount('player-price');
+    if(isNaN(playerPrice)){
+        alert('Enter a number');
+        return;
+    } 
+    else if(playerPrice<0){
+        alert('Enter a positive number');
+        return;
+    }
+    document.getElementById('player-expenses').innerText = playerPrice * (playersName.length);
+})
+
